15742efef0ce31525c5d27c6c06bc936f9f02580
ssth <srishanth471011@gmail.com>
2025-09-18 00:23:10 +0530

    added exp10 odd_or_even

--- SS & MP/cycle 2/odd_or_even.asm
+++ SS & MP/cycle 2/odd_or_even.asm
@@ -1,102 +1,130 @@
+; Even/Odd Number Checker - Linux x86 Assembly
+
 section .data
-    msg1 db 'Enter the string : ', 0
+    msg1 db 'Enter the number : ', 0
     msg1_len equ $ - msg1
     
-   
-    toggled db 'Toggled String: ', 0
-    toggled_len equ $ - toggled
+    even db 'Even' , 0 
+    even_len equ $ - even 
     
-  
-
+    odd db 'Odd' , 0 
+    odd_len equ $ - odd 
+    
     newline db 10
 
 section .bss
-    str1  resb 100
-       str1_len resb 4 
+    str1 resb 100
+    str1_len resb 4 
+    number resb 4
        
-  
 section .text
     global _start
 
 _start:
+    ; Display prompt message
     mov eax, 4
     mov ebx, 1
     mov ecx, msg1
     mov edx, msg1_len
     int 0x80
-
+    
+    ; Read input from user
     mov eax, 3
     mov ebx, 0
     mov ecx, str1
     mov edx, 100
     int 0x80
     
+    ; Null-terminate the input string
     dec eax
-      mov [str1_len] , eax 
-      
+    mov [str1_len], eax
     mov ebx, eax
     mov byte [str1 + ebx], 0
-
-    mov esi, str1
     
-.toggle_loop: 
-
-    mov al , [esi]
-    cmp al , 0 
-    je .completed
+    ; Convert string to integer
+    call stoi
+    cmp eax, -1
+    je exit
     
-    cmp al , 'A'
-    jb .check_lowercase
+    ; Check if number is even or odd
+    mov [number], eax 
+    test eax, 1
+    jz is_even
     
-    cmp al , 'Z'
-    ja .check_lowercase 
+is_odd: 
+    mov eax, 4 
+    mov ebx, 1 
+    mov ecx, odd 
+    mov edx, odd_len 
+    int 0x80 
     
-    add al , 32 
-    mov [esi] , al 
+    mov eax, 4
+    mov ebx, 1 
+    mov ecx, newline 
+    mov edx, 1 
+    int 0x80 
+    jmp exit 
     
-    jmp .next_char
+is_even: 
+    mov eax, 4 
+    mov ebx, 1 
+    mov ecx, even 
+    mov edx, even_len 
+    int 0x80 
     
-
-.check_lowercase: 
-    cmp al , 'a'
-    jb .next_char
-    cmp al , 'z'
-    ja .next_char 
+    mov eax, 4
+    mov ebx, 1 
+    mov ecx, newline 
+    mov edx, 1 
+    int 0x80 
+    jmp exit 
     
-    sub al , 32 
-    mov [esi] , al 
+; String to Integer conversion function
+stoi: 
+    push esi 
+    push ebx 
+    push ecx 
+    push edx 
     
-    jmp .next_char
+    mov esi, str1 
+    xor eax, eax 
+    xor ecx, ecx 
+    mov ecx, 10
     
+    jmp convert_loop 
     
-.next_char: 
-
+convert_loop: 
+    mov bl, [esi]
+    cmp bl, 0 
+    je .conversion_done
+    
+    ; Validate digit
+    cmp bl, '0'
+    jb .invalid_input 
+    cmp bl, '9'
+    ja .invalid_input 
+    
+    sub bl, '0'
+    mul ecx     
+    add eax, ebx 
+    
     inc esi 
-    jmp .toggle_loop
+    jmp convert_loop
     
-.completed: 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, toggled
-    mov edx, toggled_len
-    int 0x80
+.conversion_done: 
+    jmp stoi_exit
     
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, str1
-    mov edx, str1_len
-    int 0x80
+.invalid_input:
+    mov eax, -1
     
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, newline
-    mov edx, 1
-    int 0x80
-
+stoi_exit: 
+    pop edx 
+    pop ecx 
+    pop ebx 
+    pop esi 
+    ret
     
-.exit:
+exit:
     mov eax, 1
     mov ebx, 0
     int 0x80
-
- 
