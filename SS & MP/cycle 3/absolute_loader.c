5f18f5f0c249a3dd14cb831ed94bb5630abc087c
Srishanth57 <srishanth471011@gmail.com>
2025-10-26 17:34:37 +0530

    updated loader prog

--- SS & MP/cycle 3/absolute_loader.c
+++ SS & MP/cycle 3/absolute_loader.c
@@ -4,55 +4,26 @@
 
 int main()
 {
-    FILE *object;
-    object = fopen("object.txt", "r");
-    int count = 0, addr;
-    char line[100];
-
-    char *token;
-    const char *delimiter = "^";
-    printf("================================================\n");
-    printf("|\t\t\tMemory\t\t\t|\n");
-    printf("================================================\n");
+    FILE *object = fopen("object.txt", "r");
+    char line[100], obj_code[70];
+    unsigned int addr, len;
+    printf("Memory\n");
     while (fgets(line, 100, object) != NULL)
     {
-
         if (line[0] == 'T')
         {
-            int field_count = 0;
+            sscanf(line, "T^%6X^%2X^%s", &addr, &len, obj_code);
+            printf("%04X  ", addr);
 
-            token = strtok(line, delimiter);
-            while (token != NULL)
+            for (int i = 0; i < len * 2; i += 2)
             {
-                if (field_count == 1)
-                {
-                    addr = (int)strtol(token, NULL, 16);
-
-                    printf("%s\t", token);
-                }
-
-                if (field_count >= 3)
-                {
-
-                    int flag = 1;
-                    for (int i = 0; token[i] != '\0'; i++)
-                    {
-                        if (token[i] != '0')
-                        {
-                            flag = 0;
-                            break;
-                        }
-                    }
-                    if (!flag)
-                    {
-                        printf("%08s\t", token);
-                    }
-                }
-                field_count++;
-                token = strtok(NULL, delimiter);
+                printf("%c%c", obj_code[i], obj_code[i + 1]);
+                if ((i+2) % 8 == 0)
+                    printf(" ");
             }
             printf("\n");
         }
     }
+    fclose(object);
     return 0;
 }
