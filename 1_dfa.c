9be88af0a7fffb5ae905025031b9ea7f7b081bde
Srishanth57 <srishanth471011@gmail.com>
2026-07-08 01:48:56 +0530

    Update: Improvement & Optimization

--- 1_dfa.c
+++ 1_dfa.c
@@ -1,155 +1,148 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define MAX 100
+#define MAX_STATES 20
+#define MAX_TRANSITIONS_PER_STATE 10
 
-typedef struct DFA {
-    int nos;                  // Number of states
-    int noi;                  // Number of input symbols
-    int nof;                  // Number of final states
-    int delta[10][10];        // Transition function
-    int final[10];            // Final states
-} DFA;
+typedef struct transn
+{
+    size_t to;
+    char symbol;
+} transition;
 
-int main() {
+typedef struct dfa
+{
+    size_t numberOfStates;
+    size_t numberOfFinalStates;
+    char inputSymbols[MAX_STATES];
+    int finalStates[MAX_STATES];
+    // 2D Array: transition[from_state][transition_index]
+    /*
 
-    DFA d;
+    [
+    0: [[1, 'a'], [0, 'b']],
+    1: [[0, 'a'], [1, 'b']]
+    ]
 
-    printf("\nEnter no of states: ");
-    scanf("%d", &d.nos);
+    */
+    transition transition[MAX_STATES][MAX_TRANSITIONS_PER_STATE];
+} dfa;
 
-    printf("\nEnter no of input symbols: ");
-    scanf("%d", &d.noi);
+size_t validateInputString(dfa d, int i)
+{
+    // Input String
+    char str[100];
+    printf("Enter string: ");
+    scanf(" %[^\n]", str); // Note the space before %
 
-    getchar();
+    size_t currentState = 0;
 
-    // Store input symbols as one string
-    char inputSymbols[MAX];
-
-    printf("Enter input symbols separated by spaces: ");
-    fgets(inputSymbols, sizeof(inputSymbols), stdin);
-
-    inputSymbols[strcspn(inputSymbols, "\n")] = '\0';
-
-
-    printf("\nEnter no of final states: ");
-    scanf("%d", &d.nof);
-
-    // Store final states
-    for(int i = 0; i < d.nof; i++) {
-        printf("Enter final state no %d: ", i + 1);
-        scanf("%d", &d.final[i]);
-    }
-
-
-    // Read transition table
-    printf("\nEnter transitions:\n");
-
-    for(int i = 0; i < d.nos; i++) {
-
-        char temp[MAX];
-        strcpy(temp, inputSymbols);
-
-        char *token = strtok(temp, " ");
-
-        int j = 0;
-
-        while(token != NULL && j < d.noi) {
-
-            printf("d(q%d,%s): ", i, token);
-            scanf("%d", &d.delta[i][j]);
-
-            token = strtok(NULL, " ");
-            j++;
-        }
-    }
-
-
-   // Check input strings using do-while
-
-char string[MAX];
-
-do {
-
-    printf("\nEnter string to check: ");
-    scanf("%s", string);
-
-    int currentState = 0;
-    int valid = 1;
-
-    int i = 0;
-
-    // Traverse the string
-    while(string[i] != '\0') {
-
-        char symbol = string[i];
-
-        int symbolIndex = -1;
-
-
-        // Find symbol index
-        char temp[MAX];
-        strcpy(temp, inputSymbols);
-
-        char *token = strtok(temp, " ");
-
-        int index = 0;
-
-
-        while(token != NULL) {
-
-            if(token[0] == symbol) {
-                symbolIndex = index;
+    char *p1 = str;
+    while (*p1 != '\0')
+    {
+        bool symbolFound = false;
+        for (size_t j = 0; j < i; j++)
+        {
+            if (*p1 == d.inputSymbols[j])
+            {
+                currentState = d.transition[currentState][j].to;
+                symbolFound = true;
                 break;
             }
-
-            token = strtok(NULL, " ");
-            index++;
         }
 
+        if (!symbolFound)
+        {
+            printf("\n------------------Invalid character %c------------------\n\n", *p1);
+            exit(0);
+        }
 
-        // Invalid symbol
-        if(symbolIndex == -1) {
+        p1++;
+    }
 
-            printf("Invalid symbol '%c' in string\n", symbol);
-            valid = 0;
+    return currentState;
+}
+
+int main()
+{
+
+    dfa d = {0};
+
+    // States
+    printf("Number of states: ");
+    scanf("%zu", &d.numberOfStates);
+
+    // Input Symbols
+    char temp[MAX_STATES];
+    printf("Input symbols - Q: ");
+    scanf(" %[^\n]", temp); // Note the space before %. Use to read a string and replace the newline with \0 automatically.
+
+    int i = 0;
+    for (char *ptr = temp; *ptr != '\0'; ptr++)
+    {
+        if (*ptr == ' ')
+            continue;
+        d.inputSymbols[i++] = *ptr;
+    }
+
+    // Final States
+    printf("Number of final states: ");
+    scanf("%zu", &d.numberOfFinalStates);
+
+    printf("Enter final states: ");
+    for (size_t f = 0; f < d.numberOfFinalStates; f++)
+    {
+        scanf("%zu", &d.finalStates[f]);
+    }
+
+    // Transition function
+    for (size_t j = 0; j < d.numberOfStates; j++)
+    {
+        for (size_t k = 0; k < i; k++)
+        {
+            printf("delta(q%zu,%c): ", j, d.inputSymbols[k]);
+            scanf("%zu", &d.transition[j][k].to);
+
+            d.transition[j][k].symbol = d.inputSymbols[k];
+        }
+    }
+
+    // Menu driven
+    while (1)
+    {
+        int value;
+        bool accepted = false;
+        printf("\n\nChoose an option\n1. Enter string\n2. Exit\n");
+        scanf("%d", &value);
+        switch (value)
+        {
+        case 1:
+        {
+            size_t currentState = validateInputString(d, i);
+
+            accepted = false;
+            for (size_t k = 0; k < d.numberOfFinalStates; k++)
+            {
+                if (currentState == d.finalStates[k])
+                    accepted = true;
+            }
+
+            printf("\n");
+            if (accepted)
+                printf("------------------String accepted------------------");
+            else
+                printf("------------------String rejected------------------");
             break;
-
         }
-
-
-        // DFA transition
-        currentState = d.delta[currentState][symbolIndex];
-
-        i++;
-    }
-
-
-    // Check final state
-    int accepted = 0;
-
-    for(int i = 0; i < d.nof; i++) {
-
-        if(currentState == d.final[i]) {
-            accepted = 1;
+        case 2:
+            printf("\nClosing program....\n");
+            exit(0);
+            break;
+        default:
+            printf("Invalid choice");
             break;
         }
-
     }
-
-
-    if(valid) {
-
-        if(accepted)
-            printf("String Accepted\n");
-
-        else
-            printf("String Rejected\n");
-
-    }
-
-
-} while(1);
-
     return 0;
 }
